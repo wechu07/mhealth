774d3c7de1e1494e5f84ffd91402a9ec66aa66b5
wechu07 <wechu017@gmail.com>
2023-09-09 13:22:15 +0300

    update pages navigation and theme

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -1,55 +1,68 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.render("home");
+  const page = "Home";
+  res.render("home", { page });
 });
 
 router.get("/about", (req, res) => {
-  res.render("about");
+  const page = "About Us";
+  res.render("about", { page });
 });
 
 router.get("/team", (req, res) => {
-  res.render("team");
+  const page = "Meet The Team";
+  res.render("team", { page });
 });
 
 router.get("/focus", (req, res) => {
-  res.render("focus");
+  const page = "Our Focus Areas";
+  res.render("focus", { page });
 });
 
 router.get("/signup", (req, res) => {
-  res.render("signup");
+  const page = "Member Signup";
+  res.render("signup", { page });
 });
 
 router.get("/members", (req, res) => {
-  res.render("members");
+  const page = "See our Members";
+  res.render("members", { page });
 });
 
 router.get("/fundraising", (req, res) => {
-  res.render("fundraising");
+  const page = "Fundraising";
+  res.render("fundraising", { page });
 });
 
 router.get("/partner", (req, res) => {
-  res.render("partner");
+  const page = "Partner With Us";
+  res.render("partner", { page });
 });
 
 router.get("/stakeholders", (req, res) => {
-  res.render("stakeholders");
+  const page = "Stakeholders";
+  res.render("stakeholders", { page });
 });
 
 router.get("/gallery", (req, res) => {
-  res.render("gallery");
+  const page = "Gallery";
+  res.render("gallery", { page });
 });
 
 router.get("/resources", (req, res) => {
-  res.render("resources");
+  const page = "Resources";
+  res.render("resources", { page });
 });
 
 router.get("/donate", (req, res) => {
-  res.render("donate");
+  const page = "Donate";
+  res.render("donate", { page });
 });
 
 router.get("/contact", (req, res) => {
-  res.render("contact");
+  const page = "Contact";
+  res.render("contact", { page });
 });
 
 module.exports = router;
